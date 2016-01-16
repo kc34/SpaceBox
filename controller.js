25151688386b407396572ad50e0fc92dea8a3497
Kevin Chang <kc34@rice.edu>
2016-01-16 15:00:24 -0600

    Primitive panning and scrolling

--- controller.js
+++ controller.js
@@ -2,17 +2,66 @@
 class Controller {
     constructor() {
         this.name = "I am a controller." // Placeholder name.
-
+		this.mousedown = false;
+		this.last_mouse_location = null;
+		this.mouse_travelled = null;
+		this.MOUSE_TRAVEL_THRESHOLD = 10;
     }
 
-
-    static keydown_handler(key) {
-        console.log(key);
+    keydown_handler(key_event) {
+		var keynum = window.event ? key_event.keyCode : key_event.which; // window.event = userIsIE
+		var key = String.fromCharCode(keynum);
+		if (key == "&") { // Up arrow
+			my_view.scale /= 2;
+			console.log(my_view.scale);
+		} else if (key == "(") { // Down arrow
+			my_view.scale *= 2;
+			console.log(my_view.scale);
+		}
+		console.log(key);
     }
     
-    static mousedown_handler(event) {
-		var canvas_x = event.x - canvas.offsetLeft;
-		var canvas_y = event.y - canvas.offsetTop;
-		my_model.add(canvas_x, canvas_y);
+    click_handler(event) {
+		// Do nothing ... for now.
+	}
+    
+	mousedown_handler(event) {
+		this.mousedown = true;
+		this.last_mouse_location = { x : event.x, y : event.y }
+		this.mouse_travelled = 0;
+
+	}
+	
+	mouseup_handler(event) {
+		this.mousedown = false;
+		this.mousedown_location = null;
+		
+		console.log(this.MOUSE_TRAVEL_THRESHOLD);
+		
+		if (this.mouse_travelled < this.MOUSE_TRAVEL_THRESHOLD) {
+			console.log("Star adding");
+			var x = ((event.x - canvas.offsetLeft) - ( window.innerWidth / 2 + my_view.center.x )) * my_view.scale;
+			var y = ((event.y - canvas.offsetTop) - ( window.innerHeight / 2 + my_view.center.y )) * my_view.scale;
+			my_model.add(x,  y);
+		
+		}
+		
+		console.log("Mouse travel:", this.mouse_travelled);
+		this.mouse_travelled = null;
+	}
+	
+	mousemove_handler(event) {
+		if (this.mousedown == true) {
+			var mouse_delta = {
+				x : event.x - this.last_mouse_location.x,
+				y : event.y - this.last_mouse_location.y
+			}
+			this.mouse_travelled += Math.abs(mouse_delta.x) + Math.abs(mouse_delta.y);
+			this.last_mouse_location.x = event.x;
+			this.last_mouse_location.y = event.y;
+			my_view.center.x += mouse_delta.x;
+			my_view.center.y += mouse_delta.y;
+		}
+		
 	}
 }
--- view.js
+++ view.js
@@ -2,17 +2,19 @@
 class View {
     constructor() {
         this.name = "I am a view." // Placeholder name.
+        this.center = { x : 0, y : 0 }
+        this.scale = 1;
     }
 
-    static draw() {
+    draw() {
         ctx.fillStyle = "#000000";
 		ctx.fillRect( 0 , 0 , window.innerWidth , window.innerHeight );
 		
 		ctx.fillStyle = "#FFFF00";
 		for (var obj in my_model.box) {
-			var x = my_model.box[obj].x;
-			var y = my_model.box[obj].y;
-			var radius = 5;
+			var x = (my_view.center.x + window.innerWidth / 2 + my_model.box[obj].x / this.scale);
+			var y = (my_view.center.y + window.innerHeight / 2 + my_model.box[obj].y / this.scale);
+			var radius = 10 / this.scale;
 			var startAngle = 0;
 			var endAngle = 2 * Math.PI;
 			var anticlockwise = false;
