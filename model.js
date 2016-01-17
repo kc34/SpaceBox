0d1309e80a4ea8571c40e8271720b172dade2f03
awesmond <ow4@rice.edu>
2016-01-16 20:35:09 -0600

    edited neighbor, acceleration, createOrbitables

--- model.js
+++ model.js
@@ -33,6 +33,7 @@ Sector.prototype.addBody = function(x,y,t,v) { // Remind Kevin to edit values
 	}
 }
 Sector.prototype.update = function(dt) {
+	for (idx in Sector.bodies)
 }
 Sector.prototype.get_bodies = function() {
 	return this.bodies;
@@ -43,10 +44,10 @@ Sector.prototype.collision_update = function(){
 	for (idx in Sector.bodies) {
 		for (idx2 in Sector.bodies) {
 			if (idx != idx2) {
-				var x1 = Sector.bodies[idx].x_position
-				var x2 = Sector.bodies[idx2].x_position
-				var y1 = Sector.bodies[idx].y_position
-				var y2 = Sector.bodies[idx2].y_position // Remind Kevin to add index.
+				var x1 = Sector.bodies[idx].x_position;
+				var x2 = Sector.bodies[idx2].x_position;
+				var y1 = Sector.bodies[idx].y_position;
+				var y2 = Sector.bodies[idx2].y_position; // Remind Kevin to add index.
 				var x_dist = (x1-x2);
 				var y_dist = (y1-y2);
 				var t_dist = Math.pow(Math.pow(x_dist, 2) + Math.pow(y_dist, 2), 0.5); // ** doesn't work i guess.
@@ -54,12 +55,12 @@ Sector.prototype.collision_update = function(){
 					if (typeof(Sector.bodies[idx]) == Star) {
 						Sector.remove(Sector.bodies[idx2]);
 						var coor = (x2,y2);
-						col_array[0].push(coor)
+						col_array[0].push(coor);
 					} 
 					else if (typeof(Sector.bodies[idx2]) == Star){
 						Sector.remove(Sector.bodies[idx]);
 						var coor = (x1,y1);
-						col_array[0].push(coor)
+						col_array[0].push(coor);
 					}
 					else {
 						Sector.remove(Sector.bodies[idx2]);
@@ -76,12 +77,33 @@ Sector.prototype.collision_update = function(){
 	return col_array;
 }
 
-Sector.prototype.neighbor = function(x, y) {
+var createOrbitables = function(bodytype) {
+	orbitables = [];
+	if (bodytype == 'Planet') {
+		for (idx in Sector.bodies) {
+			if (typeof Sector.bodies[idx] == 'Star') {
+				orbitables.push(Sector.bodies[idx]);
+			}
+		}
+	}
+	elseif (bodytype == 'Moon') {
+		for (idx in Sector.bodies) {
+			if (typeof Sector.bodies[idx] == 'Star' || typeof Sector.bodies == 'Planet') {
+				orbitables.push(Sector.bodies[idx]);
+			}
+		}
+	}
+	return orbitables;
+}
+	
+	
+Sector.prototype.neighbor = function(body) {
 	var dist_array = [];
 	var neighbor_array = [];
-	for (idx in Sector.bodies) {
-		var x1 = x - Sector.bodies[idx].x_position;
-		var y1 = y - Sector.bodies[idx].y_position;
+	orbitables = createOrbitables(typeof body)
+	for (idx in orbitables) {
+		var x1 = body.x_position - Sector.bodies[idx].x_position;
+		var y1 = body.y_position - Sector.bodies[idx].y_position;
 		var dist = Math.pow(Math.pow(x1, 2) + Math.pow(y1, 2), 0.5);
 		if (dist != 0){
 			dist_array.push(dist);
@@ -90,7 +112,15 @@ Sector.prototype.neighbor = function(x, y) {
 	}
 	var min_dist = math.min(dist_array);
 	var n_idx = dist_array.indexOf(min_dist);
-	return neighbor_array[n_idx];
+	return [neighbor_array[n_idx], x1, y1, dist];
+}
+
+// calculates the acceleration for body1 (body2 is unaffected by this acceleration)
+Sector.prototype.acceleration = function(body1, body2, dist, x1, y1) {
+	var magnitude = body1.mass * body2.mass / Math.pow(dist,2);
+	var cos = -x1/dist;
+	var sin = -y1/dist;
+	return [magnitude*cos, magnitude*sin]
 }
 
 var Body = function(x, y, t, v) {
