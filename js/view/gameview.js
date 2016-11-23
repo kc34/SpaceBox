7506ae7d23c649f59e9cd74e40f4d3a491515878
Kevin Chang <kc34@rice.edu>
2016-11-23 07:13:22 -0600

    More interface fun.

--- js/view/gameview.js
+++ js/view/gameview.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**
- * TODO: Throw out layers, have a camera?
+ * TODO: Fix keys (not very important)
  */
 var GameView = function(model) {
 	this.model = model;
@@ -31,95 +31,6 @@ var GameView = function(model) {
 
   this.touchCache = [];
 
-	/**
-	 * This function will draw everything!
-	 */
-	this.drawPanel = function(ctx, windowX, windowY) {
-
-    // Start by drawing the background (and clearing the screen!);
-		this.drawBackground(ctx);
-
-    // Draw each object.
-		myModel.getBodies().forEach(function(obj) {
-      obj.accept(DrawAlgo, [ctx, this]);
-    }, this);
-
-    // Draw each explosion
-    myModel.explosions.forEach(function(obj) {
-      var screenVector = this.modelToViewCoordinate(obj.positionVector);
-      var explosionRadius = 50 / this.getScale();
-      GameView.drawImageAt(ctx, this.explosionFrames[obj.getSkinID()], screenVector, explosionRadius);
-    }, this);
-
-    /*
-    If the mouse has been down past the panning threshold, then the user is trying
-    to create a new Body. Check for this, and draw the ghost body as needed.
-    */
-		var t = new Date();
-		t -= this.mousedownTime;
-		t /= 1000;
-		if (t > 0.25) {
-      if (this.mouseState == "MOVE" || this.mouseState == "DOWN") {
-        if (this.mouseState == "MOVE") {
-    			ctx.strokeStyle = "#FFFFFF";
-    			ctx.beginPath();
-    			ctx.moveTo(this.mousedownLocation.x, this.mousedownLocation.y);
-    			ctx.lineTo(this.mouseLocation.x, this.mouseLocation.y);
-    			ctx.stroke();
-  		  } else if (this.mouseState == "DOWN") {
-          this.newBodyTime = t;
-  			}
-        var modelVector = this.viewToModelCoordinate(this.mousedownLocation);
-        var ghostObject = GameView.createBody(modelVector, Vector.ZERO, this.newBodyTime, this.rand);
-        ghostObject.accept(DrawAlgo, [ctx, this]);
-      }
-		}
-
-  }
-
-  this.drawBackground = function(ctx) {
-
-    ctx.globalAlpha = 1;
-
-    var pictureSize = 20000 / this.getZScale(this.backgroundZ);
-    var screenVector = this.zToViewCoordinate(this.backgroundZ, Vector.fromComponents(0, 0));
-
-    for (var i = -10; i < 11; i++) {
-      for (var j = -10; j < 11; j++) {
-        GameView.drawImageAt(ctx, this.backgroundImage, screenVector.add(Vector.fromComponents(i, j).scMult(pictureSize)), pictureSize / 2);
-      }
-    }
-
-    ctx.globalAlpha = 0.6;
-
-    var pictureSize = 10000 / this.getZScale(this.backgroundZ / 2);
-    var screenVector = this.zToViewCoordinate(this.backgroundZ / 2, Vector.fromComponents(pictureSize / 4, pictureSize / 10));
-
-    for (var i = -10; i < 11; i++) {
-      for (var j = -10; j < 11; j++) {
-        GameView.drawImageAt(ctx, this.backgroundImage, screenVector.add(Vector.fromComponents(i, j).scMult(pictureSize)), pictureSize / 2);
-      }
-    }
-    ctx.globalAlpha = 1;
-	}
-
-	/**
-	 * Adjusts GameView center and scale s.t. screenVector will stay
-	 * consistent with planeVector, but scale will change by
-	 * scalingFactor.
-	 */
-	this.zoomAt = function(screenVector, zoom) {
-    if (this.camera.z * zoom < 10)
-      return;
-    if (this.camera.z * zoom > 25000)
-      return;
-    var mouseCoordinate = this.viewToModelCoordinate(screenVector);
-    var mouseOffset = mouseCoordinate.subtract(this.getCenter());
-    var scaledOffset = mouseOffset.scMult(zoom);
-    this.setCenter(mouseCoordinate.subtract(scaledOffset));
-    this.setScale(this.getScale() * zoom);
-	}
-
 	this.keyToFunctionMap = {
 		"&" : function(gameView) { gameView.scale /= 1.1; },
 		"(" : function(gameView) { gameView.scale *= 1.1; },
@@ -133,13 +44,9 @@ var GameView = function(model) {
 
 GameView.prototype = Object.create(Panel.prototype);
 
-GameView.prototype.draw = function(ctx, offsetX, offsetY) {
-  Panel.prototype.draw.call(this, ctx, 0, 0);
-}
-
-GameView.prototype.pan = function(deltaVector) {
-  this.setCenter(this.getCenter().subtract(deltaVector));
-}
+/*
+Getters and Setters
+*/
 
 GameView.prototype.getScale = function() {
   return this.camera.z / (window.innerWidth / 2);
@@ -169,6 +76,119 @@ GameView.prototype.getZScale = function(z) {
   return (z + this.camera.z) / (window.innerWidth / 2);
 }
 
+/*
+Semi-getters and Setters
+*/
+
+GameView.prototype.pan = function(deltaVector) {
+  this.setCenter(this.getCenter().subtract(deltaVector));
+}
+
+/**
+ * Adjusts GameView center and scale s.t. screenVector will stay
+ * consistent with planeVector, but scale will change by
+ * scalingFactor.
+ */
+GameView.prototype.zoomAt = function(screenVector, zoom) {
+  if (this.camera.z * zoom < 10)
+    return;
+  if (this.camera.z * zoom > 25000)
+    return;
+  var mouseCoordinate = this.viewToModelCoordinate(screenVector);
+  var mouseOffset = mouseCoordinate.subtract(this.getCenter());
+  var scaledOffset = mouseOffset.scMult(zoom);
+  this.setCenter(mouseCoordinate.subtract(scaledOffset));
+  this.setScale(this.getScale() * zoom);
+}
+
+/*
+Drawing
+*/
+
+GameView.prototype.draw = function(ctx, offsetX, offsetY) {
+  Panel.prototype.draw.call(this, ctx, 0, 0);
+}
+
+/**
+ * This function will draw everything!
+ */
+GameView.prototype.drawPanel = function(ctx, windowX, windowY) {
+
+  // Start by drawing the background (and clearing the screen!);
+	this.drawBackground(ctx);
+
+  // Draw each object.
+	myModel.getBodies().forEach(function(obj) {
+    obj.accept(DrawAlgo, [ctx, this]);
+  }, this);
+
+  // Draw each explosion
+  myModel.explosions.forEach(function(obj) {
+    var screenVector = this.modelToViewCoordinate(obj.positionVector);
+    var explosionRadius = 50 / this.getScale();
+    GameView.drawImageAt(ctx, this.explosionFrames[obj.getSkinID()], screenVector, explosionRadius);
+  }, this);
+
+  /*
+  If the mouse has been down past the panning threshold, then the user is trying
+  to create a new Body. Check for this, and draw the ghost body as needed.
+  */
+	var t = new Date();
+	t -= this.mousedownTime;
+	t /= 1000;
+	if (t > 0.25) {
+    if (this.mouseState == "MOVE" || this.mouseState == "DOWN") {
+      if (this.mouseState == "MOVE") {
+  			ctx.strokeStyle = "#FFFFFF";
+  			ctx.beginPath();
+  			ctx.moveTo(this.mousedownLocation.x, this.mousedownLocation.y);
+  			ctx.lineTo(this.mouseLocation.x, this.mouseLocation.y);
+  			ctx.stroke();
+		  } else if (this.mouseState == "DOWN") {
+        this.newBodyTime = t;
+			}
+      var modelVector = this.viewToModelCoordinate(this.mousedownLocation);
+      var ghostObject = GameView.createBody(modelVector, Vector.ZERO, this.newBodyTime, this.rand);
+      ghostObject.accept(DrawAlgo, [ctx, this]);
+    }
+	}
+
+}
+
+GameView.prototype.drawBackground = function(ctx) {
+
+  ctx.globalAlpha = 1;
+
+  var pictureSize = 20000 / this.getZScale(this.backgroundZ);
+  var screenVector = this.zToViewCoordinate(this.backgroundZ, Vector.fromComponents(0, 0));
+
+  for (var i = -10; i < 11; i++) {
+    for (var j = -10; j < 11; j++) {
+      GameView.drawImageAt(ctx, this.backgroundImage, screenVector.add(Vector.fromComponents(i, j).scMult(pictureSize)), pictureSize / 2);
+    }
+  }
+
+  ctx.globalAlpha = 0.6;
+
+  var pictureSize = 10000 / this.getZScale(this.backgroundZ / 2);
+  var screenVector = this.zToViewCoordinate(this.backgroundZ / 2, Vector.fromComponents(pictureSize / 4, pictureSize / 10));
+
+  for (var i = -10; i < 11; i++) {
+    for (var j = -10; j < 11; j++) {
+      GameView.drawImageAt(ctx, this.backgroundImage, screenVector.add(Vector.fromComponents(i, j).scMult(pictureSize)), pictureSize / 2);
+    }
+  }
+  ctx.globalAlpha = 1;
+}
+
+/*
+Event handlers
+*/
+
+GameView.prototype.clickHandler = function(event) {
+  // Used to be a link to the about page!
+}
+
 GameView.prototype.mousedownHandler = function(event) {
   event = new Vector({x : event.clientX, y : event.clientY});
   this.mouseState = "DOWN";
@@ -178,35 +198,6 @@ GameView.prototype.mousedownHandler = function(event) {
   this.mouseLocation = event;
 
   this.rand = Math.random();
-}
-
-GameView.prototype.keydownHandler = function(keyEvent) {
-  var keynum = window.event ? keyEvent.keyCode : keyEvent.which; // window.event = userIsIE
-  var key = String.fromCharCode(keynum);
-  this.keyToFunctionMap[key](this);
-}
-
-GameView.prototype.clickHandler = function(event) {
-  // Used to be a link to the about page!
-}
-
-GameView.prototype.mouseupHandler = function(event) {
-  event = new Vector({x : event.clientX, y : event.clientY});
-  if (this.mouseState == "DOWN") {
-    this.newBodyTime = (new Date() - this.mousedownTime) / 1000;
-    var vector = this.viewToModelCoordinate(event);
-    var newBody = GameView.createBody(vector, Vector.ZERO, this.newBodyTime, this.rand);
-    myModel.addBody(newBody);
-  } else if (this.mouseState == "MOVE") {
-    var posVector_1 = this.viewToModelCoordinate(this.mousedownLocation);
-    var posVector_2 = this.viewToModelCoordinate(event);
-    var deltaVector = posVector_2.subtract(posVector_1);
-    var newBody = GameView.createBody(posVector_1, deltaVector, this.newBodyTime, this.rand);
-    myModel.addBody(newBody);
-
-  }
-  this.mouseState = "UP";
-
 }
 
 GameView.prototype.mousemoveHandler = function(event) {
@@ -232,12 +223,38 @@ GameView.prototype.mousemoveHandler = function(event) {
   this.mouseLocation = event;
 }
 
+GameView.prototype.mouseupHandler = function(event) {
+  event = new Vector({x : event.clientX, y : event.clientY});
+  if (this.mouseState == "DOWN") {
+    this.newBodyTime = (new Date() - this.mousedownTime) / 1000;
+    var vector = this.viewToModelCoordinate(event);
+    var newBody = GameView.createBody(vector, Vector.ZERO, this.newBodyTime, this.rand);
+    myModel.addBody(newBody);
+  } else if (this.mouseState == "MOVE") {
+    var posVector_1 = this.viewToModelCoordinate(this.mousedownLocation);
+    var posVector_2 = this.viewToModelCoordinate(event);
+    var deltaVector = posVector_2.subtract(posVector_1);
+    var newBody = GameView.createBody(posVector_1, deltaVector, this.newBodyTime, this.rand);
+    myModel.addBody(newBody);
+
+  }
+  this.mouseState = "UP";
+
+}
+
 GameView.prototype.mousewheelHandler = function(event) {
   if (event.wheelDelta > 0) {
     this.zoomAt(Vector.fromComponents(event.clientX, event.clientY), 1 / this.scalingFactor);
   } else {
     this.zoomAt(Vector.fromComponents(event.clientX, event.clientY), this.scalingFactor);
   }
+}
+
+
+GameView.prototype.keydownHandler = function(keyEvent) {
+  var keynum = window.event ? keyEvent.keyCode : keyEvent.which; // window.event = userIsIE
+  var key = String.fromCharCode(keynum);
+  this.keyToFunctionMap[key](this);
 }
 
 GameView.prototype.touchstartHandler = function(event) {
@@ -284,11 +301,16 @@ GameView.prototype.touchendHandler = function(event) {
   this.touchCache = this.touchCache.filter(function(touchEvent) {
     return touchEvent.identifier != event.identifier
   }, this);
+  // Just to double-check
   if (this.touchCache.length == 0) {
     this.mouseState = "UP";
   }
   console.log(this.touchCache);
 }
+
+/*
+Helper functions to scale things
+*/
 
 GameView.prototype.modelToViewCoordinate = function(modelVector) {
 	/**
@@ -316,6 +338,10 @@ GameView.prototype.zToViewCoordinate = function(z, backgroundVector) {
 	var viewVector = backgroundVector.subtract(this.getBackgroundCenter()).scMult(1 / this.getZScale(z)).add(viewCenter);
 	return viewVector;
 }
+
+/*
+Static functions
+*/
 
 GameView.timeToRadius = function(t) {
 	if (t < 1) {
--- js/view/view.js
+++ js/view/view.js
@@ -12,27 +12,60 @@ var View = function(model) {
 	this.addComponent("gamePanel", gamePanel);
 
 	// Draw the button that leads to the about page.
-	var aboutButton = new Panel(10, window.innerHeight - 30, 20, 20);
+	var aboutButton = new Panel(20, window.innerHeight - 60, 40, 40);
 	aboutButton.color = "#888888";
 	aboutButton.borderColor = "#AAAAAA";
 	aboutButton.postprocess = function(ctx, offsetX, offsetY) {
 		ctx.fillStyle = "#FFFFFF";
-		ctx.font = "20px Arial"
-		ctx.fillText("?", 4 + offsetX, 16.25 + offsetY);
+		ctx.font = "40px Arial"
+		ctx.fillText("?", 8 + offsetX, 32.5 + offsetY);
 	}
 	aboutButton.clickHandler = function(event) {
 		window.location = "html/about.html";
 	};
+  aboutButton.touchstartHandler = function(event) {
+    this.clickHandler(event);
+  }
 
 	this.addComponent("aboutButton", aboutButton);
 
-	var undoButton = new Panel(40, window.innerHeight - 30, 20, 20);
-	undoButton.color = "#FF0000";
+	var undoButton = new Panel(80, window.innerHeight - 60, 40, 40);
+	undoButton.color = "#888888";
+	undoButton.postprocess = function(ctx, offsetX, offsetY) {
+		ctx.fillStyle = "#FFFFFF";
+		ctx.font = "40px Arial"
+		ctx.fillText("<", 8 + offsetX, 32.5 + offsetY);
+	}
 	undoButton.clickHandler = function(event) {
 		myModel.bodies.pop();
 	}
+  undoButton.touchstartHandler = function(event) {
+    this.clickHandler(event);
+  }
 
 	this.addComponent("undoButton", undoButton);
+
+  var pauseButton = new Panel(140, window.innerHeight - 60, 40, 40);
+  pauseButton.color = "#888888";
+  pauseButton.postprocess = function(ctx, offsetX, offsetY) {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "35px Arial"
+    ctx.fillText("||", 11 + offsetX, 29 + offsetY);
+  }
+  pauseButton.clickHandler = function(event) {
+    if (myModel.running) {
+      myModel.running = false;
+      this.color = "#FF0000";
+    } else {
+      myModel.running = true;
+      this.color = "#888888"
+    }
+  }
+  undoButton.touchstartHandler = function(event) {
+    this.clickHandler(event);
+  }
+
+  this.addComponent("pauseButton", pauseButton);
 
 }
 
